62f7af4f08a31c38b660f912b6d2fbc7560a2077
Sharath <64036036+mrsharathps@users.noreply.github.com>
2026-01-19 22:59:04 +0530

    Time: 15 ms (77.86%) | Memory: 46.8 MB (69.33%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -2,13 +2,19 @@ class Solution {
     public static String removeKdigits(String num, int k) {
     
         int n = num.length();
+
+        //digits to remove is same as string lenth
         if(k==n){
             return "0";
         }
 
+        //stack declaration
         Stack<Character> stack = new Stack<>();
         
+        //getting individual characters of num which is converted to charArray
         for( char c : num.toCharArray()){
+
+            //last digit in stack is bigger that current character gotten from c. If so pop it
             while(!stack.isEmpty() && k > 0 && stack.peek()>c){
                 stack.pop();
                 k--;
@@ -21,12 +27,14 @@ class Solution {
             k--;
         }
 
+        //return value expects a string. hence converting stack to string
         StringBuilder sb = new StringBuilder();
         for(char c : stack){
             sb.append(c);
         }
 
         int i =0;
+        //removing the leading zero
         while(i<sb.length()-1 && sb.charAt(i) == '0'){
             i++;
         }
