6952b582f3f5ae50b13045a06947085a5006188c
Sharath <64036036+mrsharathps@users.noreply.github.com>
2026-01-30 08:11:24 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (80.87%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    //Testing
     public static List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         postOrder(root, result);
