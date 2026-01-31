d46a349a6fac0aa6f611c3934753c0379fb8ac22
Sharath <64036036+mrsharathps@users.noreply.github.com>
2026-01-31 22:21:42 +0530

    Time: 1 ms (99.82%) | Memory: 55.7 MB (42.32%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,39 @@
+// class Solution {
+//     public static int majorityElement(int[] nums) {
+//         if(nums.length == 0){ return 0;}
+
+//         Arrays.sort(nums);
+//         return nums[nums.length / 2];
+//     }
+
+//     public static void main(String[] args){
+//         int[] nums = {2,2,1,1,1,2,2};
+//         System.out.println(majorityElement(nums));
+//     }
+// }
+
+class Solution {
+    public static int majorityElement(int[] nums) {
+        int occurance = 0;
+        int result = 0;
+
+        for(int num : nums){
+            if(occurance==0){
+                result=num;
+            }
+
+            if(num==result){
+                occurance++;
+            }
+            else{
+                occurance--;}
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+}
