770647455f481f10b4b73bfe0b9243438e921cef
Sharath <64036036+mrsharathps@users.noreply.github.com>
2026-02-03 07:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (85.18%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    public static boolean isHappy(int n) {
+
+        int slow =n;
+        int fast =n;
+
+        while(true){
+            slow = nextNum(slow);
+            fast = nextNum(nextNum(fast));
+
+            if(slow == fast){
+                break;
+            }
+        }
+
+    return slow ==1;
+    }
+
+
+    private static int nextNum(int n){
+        int result = 0;
+
+        while (n>0){
+            int num = n%10;
+            result = result + num * num;
+            n= n/10;
+        }
+        return result;  
+    }
+
+
+    public static void main(String[] args){
+        int n = 19;
+        System.out.println(isHappy(n));
+    }
+}
